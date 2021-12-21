183e64daf44dd36811e7204746aadbb605263293
The Sharp Ninja <ninja@thesharp.ninja>
2021-12-20 18:31:41 -0600

    Builds packages

--- src/ArchMastery.Reflector.Core/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Reflection;
-
-
-
-[assembly: AssemblyVersion("1.2.0.0")]
-[assembly: AssemblyFileVersion("1.2.0.0")]
-[assembly: AssemblyInformationalVersion("1.2.0+97.Branch.master.Sha.861a1f54b5fc508429439d7fb01d07f79be68db9")]
-
